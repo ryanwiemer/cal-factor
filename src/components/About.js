d1271d7e23f998ae5df3e931dc899b48c4105b4e
Ryan Wiemer <ryan@ryanwiemer.com>
2017-10-02 14:38:09 -0700

    Fix scrolling

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ class About extends Component {
   scrollToAbout() {
     document.querySelector('.about').scrollIntoView({
       behavior: 'smooth',
-      block: "start"
+      block: 'start'
     });
   }
 
